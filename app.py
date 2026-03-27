d5232e32684214e04a86f506e83a563bd7851043
chitrasharath <chitrasharathchandra001@gmail.com>
2026-03-27 19:04:15 +0000

    Tic tac toe in python

--- app.py
+++ app.py
@@ -9,14 +9,47 @@ board = [
 def play(position):
     global current_player
     # your code here
+    position=int(position)
+    if position <0:
+        print("Invalid Position")
+        return
+    if position >8:
+        print("Invalid Position")
+        return
+    if board[position]=='-':
+        board[position]=current_player
+    else:
+        print("Position taken")
+        return
+    return
 
 def check_for_winner():
     # your code here
+    winning_combinations=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for i in range(len(winning_combinations)):
+        current_combination=[]
+        current_combination.append(board[winning_combinations[i][0]])
+        current_combination.append(board[winning_combinations[i][1]])
+        current_combination.append(board[winning_combinations[i][2]])
+        if "-" not in set(current_combination):
+            if len(set(current_combination)) <= 1:
+                return True
     return False
 
 def new_game():
     # your code here
-    pass
+    global current_player
+    global stop
+    global board
+
+    current_player = "X"
+    stop = False
+    board = [
+        "-","-","-",
+        "-","-","-",
+        "-","-","-",
+    ]
+    return
 
 def print_board():
     print(f"""
@@ -29,7 +62,20 @@ def print_board():
 
 while stop == False:
     command = input("What do you want?: ")
-
+    if command == "play":
+        position = input("Position:")
+        play(position)
+        if check_for_winner():
+            print("There is a winner")
+            new_game()
+        else:
+            if current_player=="X":
+                current_player="O"
+            else:
+                current_player="X"
+        print_board()
+    if command == "reset":
+        new_game()
     if command == "stop":
         stop = True
     # add commands here (if needed)
